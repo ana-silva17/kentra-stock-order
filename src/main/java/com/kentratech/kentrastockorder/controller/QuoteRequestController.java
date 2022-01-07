7cf02fc3473fda96c4f63a980cab6a673f0aa28d
Ana Silva <asfsilva87@gmail.com>
2022-01-07 14:39:50 +0000

    QuoteRequest - findAll

--- src/main/java/com/kentratech/kentrastockorder/controller/QuoteRequestController.java
+++ src/main/java/com/kentratech/kentrastockorder/controller/QuoteRequestController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/quotes")
 public class QuoteRequestController {
@@ -25,5 +27,11 @@ public class QuoteRequestController {
     public ResponseEntity<Void> delete(@PathVariable Long id){
         quoteRequestService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuoteRequest>> findAll(){
+        List<QuoteRequest> list = quoteRequestService.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/kentratech/kentrastockorder/service/QuoteRequestService.java
+++ src/main/java/com/kentratech/kentrastockorder/service/QuoteRequestService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,5 +29,10 @@ public class QuoteRequestService {
         } catch (EmptyResultDataAccessException e) {
             throw new ResourceNotFoundException(id);
         }
+    }
+
+    public List<QuoteRequest> findAll(){
+
+        return quoteRequestRepository.findAll();
     }
 }
