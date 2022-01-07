7084f814c7a93ea8fb3ec17b5c70248e75b094cb
Ana Silva <asfsilva87@gmail.com>
2022-01-07 10:01:57 +0000

    Order - findById

---
+++ src/main/java/com/kentratech/kentrastockorder/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.kentratech.kentrastockorder.repository;
+
+import com.kentratech.kentrastockorder.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>, JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/kentratech/kentrastockorder/repository/QuoteRequestRepository.java
@@ -0,0 +1,9 @@
+package com.kentratech.kentrastockorder.repository;
+
+import com.kentratech.kentrastockorder.entity.Order;
+import com.kentratech.kentrastockorder.entity.QuoteRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface QuoteRequestRepository extends PagingAndSortingRepository<QuoteRequest, Long>, JpaRepository<QuoteRequest, Long> {
+}
